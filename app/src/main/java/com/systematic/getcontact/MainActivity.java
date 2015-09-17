6f0b765f06ec87287b6a335e54b5af98d21b3455
TakersHK <colman.ng@hotmail.com>
2015-09-17 13:15:55 +0800

    Initial commit

---
+++ app/src/main/java/com/systematic/getcontact/MainActivity.java
@@ -0,0 +1,125 @@
+package com.systematic.getcontact;
+
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.provider.ContactsContract;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.widget.Button;
+        import android.widget.TextView;
+        import android.widget.Toast;
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.database.Cursor;
+
+public class MainActivity extends Activity {
+
+    Button buttonReadContact;
+    TextView textPhone;
+    TextView textContact;
+
+    final int RQS_PICKCONTACT = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonReadContact = (Button)findViewById(R.id.readcontact);
+        textPhone = (TextView)findViewById(R.id.phone);
+        textContact = (TextView)findViewById(R.id.contact);
+        buttonReadContact.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                //Start activity to get contact
+                final Uri uriContact = ContactsContract.Contacts.CONTENT_URI;
+                Intent intentPickContact = new Intent(Intent.ACTION_PICK, uriContact);
+                startActivityForResult(intentPickContact, RQS_PICKCONTACT);
+            }});
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        if(resultCode == RESULT_OK){
+            if(requestCode == RQS_PICKCONTACT){
+                Uri returnUri = data.getData();
+                Cursor cursor = getContentResolver().query(returnUri, null, null, null, null);
+
+                if(cursor.moveToNext()){
+                    int columnIndex_ID = cursor.getColumnIndex(ContactsContract.Contacts._ID);
+                    String contactID = cursor.getString(columnIndex_ID);
+
+                    int columnIndex_HASPHONENUMBER = cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER);
+                    String stringHasPhoneNumber = cursor.getString(columnIndex_HASPHONENUMBER);
+
+                    if(stringHasPhoneNumber.equalsIgnoreCase("1")){
+                        Cursor cursorNum = getContentResolver().query(
+                                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                null,
+                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactID,
+                                null,
+                                null);
+
+                        //Get the first phone number
+                        if(cursorNum.moveToNext()){
+                            String stringContact = cursorNum.getString(cursorNum.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                            int columnIndex_number = cursorNum.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                            String stringNumber = cursorNum.getString(columnIndex_number);
+                            textPhone.setText(stringNumber);
+                            textContact.setText(stringContact);
+                        }
+
+                    }else{
+                        textPhone.setText("NO Phone Number");
+                    }
+
+
+                }else{
+                    Toast.makeText(getApplicationContext(), "NO data!", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+
+
+}
+/*
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+*/
